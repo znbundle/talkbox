0c5be23b2734cb3e310c66fee01ea5ab418a8455
Vitaliy <theyamshikov@yandex.ru>
2021-01-22 21:10:03 +0600

    upd

--- src/Domain/config/container.php
+++ src/Domain/config/container.php
@@ -1,8 +1,10 @@
 <?php
 
 return [
-    'ZnBundle\TalkBox\Domain\Interfaces\Repositories\TagRepositoryInterface' => 'ZnBundle\TalkBox\Domain\Repositories\Eloquent\TagRepository',
-    'ZnBundle\TalkBox\Domain\Interfaces\Repositories\AnswerRepositoryInterface' => 'ZnBundle\TalkBox\Domain\Repositories\Eloquent\AnswerRepository',
-    'ZnBundle\TalkBox\Domain\Interfaces\Repositories\AnswerTagRepositoryInterface' => 'ZnBundle\TalkBox\Domain\Repositories\Eloquent\AnswerTagRepository',
-    'ZnBundle\TalkBox\Domain\Interfaces\Repositories\AnswerOptionRepositoryInterface' => 'ZnBundle\TalkBox\Domain\Repositories\Eloquent\AnswerOptionRepository',
+    'singletons' => [
+        'ZnBundle\TalkBox\Domain\Interfaces\Repositories\TagRepositoryInterface' => 'ZnBundle\TalkBox\Domain\Repositories\Eloquent\TagRepository',
+        'ZnBundle\TalkBox\Domain\Interfaces\Repositories\AnswerRepositoryInterface' => 'ZnBundle\TalkBox\Domain\Repositories\Eloquent\AnswerRepository',
+        'ZnBundle\TalkBox\Domain\Interfaces\Repositories\AnswerTagRepositoryInterface' => 'ZnBundle\TalkBox\Domain\Repositories\Eloquent\AnswerTagRepository',
+        'ZnBundle\TalkBox\Domain\Interfaces\Repositories\AnswerOptionRepositoryInterface' => 'ZnBundle\TalkBox\Domain\Repositories\Eloquent\AnswerOptionRepository',
+    ],
 ];
