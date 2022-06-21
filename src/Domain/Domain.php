2a32e1cc71ab6deb8a0a2eb1ba8d270198697c87
Vitaliy <theyamshikov@yandex.ru>
2022-06-21 11:51:11 +0600

    upd

--- src/Domain/Domain.php
+++ src/Domain/Domain.php
@@ -2,7 +2,7 @@
 
 namespace ZnBundle\TalkBox\Domain;
 
-use ZnCore\Domain\Interfaces\DomainInterface;
+use ZnCore\Base\Libs\Domain\Interfaces\DomainInterface;
 
 class Domain implements DomainInterface
 {
